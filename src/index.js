fdf5723976acdbc82c69acd7d24e7e82eee70289
akiko jo <akikojo@users.noreply.github.com>
2022-08-29 10:00:49 +0900

    少々JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liを生成
+  const li = document.createElement("li");
+  console.log(li);
+  alert(inputText);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
